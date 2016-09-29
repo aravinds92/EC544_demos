97825654536de9c76c11ec1a6af823b15d6c4163
Aravind <aravind4113546@gmail.com>
2016-09-29 00:54:45 -0400

    Support added for both database and file access from the js file

--- demos/chat/chat.js
+++ demos/chat/chat.js
@@ -4,10 +4,16 @@ var io = require('socket.io')(http);
 var fs = require("fs");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('mydb.db');
+//process.stdin.resume();//so the program will not close instantly
+find_date();
+calculate_time();
+fs.writeFile('./file.txt', '',  function(err) {           //Write empty string
+    if (err) {
+      return console.error(err);
+   }
+});
 
-
-var check;
-var str = '\0';										
+var str = '';										
 var time;																	//Global variable to hold the time value
 var date;                                 //Global variable to hold the time value
 var count = 0;														//Track number of messages that have come in
@@ -28,33 +34,36 @@ function find_date(){
   var year = date_local.getFullYear() 
   date = ''
   date = month + '/' + day + '/' + year
-  //console.log(now);
 }
 
 function write_to_db(source, value, time, date){
   console.log("Inside");
   db.serialize(function() {
-    db.run("CREATE TABLE items (sensor_id INTEGER PRIMARY KEY, sensor_output FLOAT, time TEXT, date TEXT)");
-    var stmt = db.prepare("INSERT INTO items VALUES(?,?,?,?)");
+    db.run("CREATE TABLE if not exists Sensor_data (sensor_id INTEGER, sensor_output FLOAT, time TEXT, date TEXT)");
+    var stmt = db.prepare("INSERT INTO Sensor_data VALUES(?,?,?,?)");
 
     stmt.run(source,value,time,date);
 
     stmt.finalize();
   });
 
-  db.close();
+  //db.close();
 }
 
-/*fs.writeFile('./file.txt', '',  function(err) {               //Clear the file at the beginning of operation. Optional
+function exitHandler() {
+    db.close();
+}
+
+fs.writeFile('./file.txt', '',  function(err) {               //Clear the file at the beginning of operation. Optional
     if (err) {
       return console.error(err);
    }
-});*/
+});
 
 
 
 setInterval(calculate_time,500);											//Call the function once every 0.5 seconds
-setInterval(find_date,10000);                      //Call the function once every 0.5 seconds
+setInterval(find_date,10000);                         //Call the function once every 10 seconds
 
 
 app.get('/', function(req, res){
@@ -67,24 +76,20 @@ io.on('connection', function(socket){
   });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    /*str = str + msg + ' ' + time + '\n'										//string to hold all the data
-    count += 1;
-    fs.writeFile('./file.txt', count + '\n',  function(err) {				//Write count at the top of the file
-   		if (err) {
-      	return console.error(err);
-   		}
-	});
-	fs.appendFile('./file.txt', str,  function(err) {						//Write the rest of the messages
+    str = msg + '\t' + time + '\t' + date + '\n'							//string to hold all the data
+	fs.appendFile('./file.txt', str,  function(err) {						//Write the new message
    	if (err) {
       return console.error(err);
    }
    });
+  console.log(str)
 
-    console.log(time);*/
-    find_date()
+    //console.log(time);
+    /*find_date()
     calculate_time()
     console.log(date)
     console.log(time)
+    write_to_db(1, 26.5, time, date)*/
   });
 });
 
@@ -92,3 +97,13 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 
 });
+
+
+/*//do something when app is closing
+process.on('exit', exitHandler());
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler());
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler());*/
